65a31d1204326989b84301eea6b17860fb969af2
Balamatiuc Gheorghe <Balamatiuc Gheorghe@DESKTOP-J23D4K1>
2023-07-09 21:44:45 +0300

    Save

--- Competition Tournament/Controllers/TeamController.cs
+++ Competition Tournament/Controllers/TeamController.cs
@@ -1,6 +1,7 @@
 ﻿using Competition_Tournament.Data;
 using Competition_Tournament.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Competition_Tournament.Controllers
 {
@@ -23,13 +24,60 @@ namespace Competition_Tournament.Controllers
             return View();
         }
 
+        public IActionResult Edit(int id)
+        {
+            var team = _context.Teams.Find(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+            return View(team);
+        }
+
         [HttpPost]
         public IActionResult Create(Team team)
         {
-            _context.Teams.Add(team);
-            _context.SaveChanges();
+            if (team.CreatedOn > DateTime.Today)
+            {
+                ModelState.AddModelError("CreatedOn", "The date cannot be in the future.");
+            }
 
+            if (ModelState.IsValid)
+            {
+                _context.Teams.Add(team);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(team);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var team = _context.Teams.Find(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+            _context.Teams.Remove(team);
+            _context.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Team team)
+        {
+            if (team.CreatedOn > DateTime.Today)
+            {
+                ModelState.AddModelError("CreatedOn", "The date cannot be in the future.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Entry(team).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(team);
         }
     }
 }
